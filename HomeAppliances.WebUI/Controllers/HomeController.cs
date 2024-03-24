8b84b107492a82abe3182a7ae0083f5d0ef52a58
alicansariboga <alicnn38@gmail.com>
2024-03-24 15:11:14 +0300

    Main Page Products.

--- HomeAppliances.WebUI/Controllers/HomeController.cs
+++ HomeAppliances.WebUI/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using HomeAppliances.Business.Abstract;
+using HomeAppliances.Entity.Concrete;
 using HomeAppliances.WebUI.Models;
 
 namespace HomeAppliances.WebUI.Controllers;
 
 public class HomeController : Controller
 {
+    private IProductService _productService;
+    public HomeController(IProductService productService)
+    {
+        _productService = productService;
+    }
     public IActionResult Index()
     {
-        return View();
+        return View(new ProductListModel()
+        {
+            Products = _productService.GetAll()
+        });
+
     }
 }
---
+++ HomeAppliances.WebUI/Models/ProductListModel.cs
@@ -0,0 +1,9 @@
+﻿using HomeAppliances.Entity.Concrete;
+
+namespace HomeAppliances.WebUI.Models
+{
+    public class ProductListModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
